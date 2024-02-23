8298630362071b84835784fb3a5b298c0f0df7aa
giamaf <gianluca.maffucci@hotmail.com>
2024-02-23 18:59:27 +0100

    added actor's class to modify movie's class

--- index.php
+++ index.php
@@ -1,4 +1,30 @@
 <?php
+class Actor
+{
+    // Proprietà
+    // Variabili d'istanza
+public $main_actor_one;
+public $main_actor_two;
+
+// Costruttore
+//! Permette di eseguire azioni nel momento in cui viene creata l'istanza della classe
+public function __construct(string $main_actor_one, string $main_actor_two)
+{
+    $this->main_actor_one = $main_actor_one;
+    $this->main_actor_two = $main_actor_two;
+}
+}
+
+// Creo un attore protagonista e un attore secondario
+$main_actor_1 = new Actor('Tobey Maguire', 'Kirsten Dunst');
+
+// Creo un attore protagonista e un attore secondario
+$main_actor_2 = new Actor('Shia LaBeouf', 'Megan Fox');
+
+// Creo un attore protagonista e un attore secondario
+$main_actor_3 = new Actor('Robert Downey Jr.', 'Gwyneth Paltrow');
+
+
 class Director
 {
     // Proprietà
@@ -37,16 +63,18 @@ public $plot;
 public $genre;
 public $year;
 public $director;
+public $actors;
 
 
 //! Permette di eseguire az// Costruttoreioni nel momento in cui viene creata l'istanza della classe
-public function __construct($title, $plot, $genre, $year, Director $director)
+public function __construct($title, $plot, $genre, $year, Director $director, Actor $actors)
 {
     $this->title = $title;
     $this->plot = $plot;
     $this->genre = $genre;
     $this->year = $year;
     $this->director = $director;
+    $this->actors = $actors;
     $this->createMovie();
 }
 
@@ -56,7 +84,8 @@ public function __construct($title, $plot, $genre, $year, Director $director)
     <strong>Trama:</strong> $this->plot<br>
     <strong>Genere:</strong> $this->genre<br>
     <strong>Anno:</strong> $this->year<br>
-    <strong>Regia:</strong> {$this->director->full_name}<hr>";
+    <strong>Regia:</strong> {$this->director->full_name}<br>
+    <strong>Attori:</strong> {$this->actors->main_actor_one}, {$this->actors->main_actor_two}<hr>";
   }
 }
 
@@ -67,13 +96,13 @@ $plot_2 = "Iron Man, è un film del 2008 diretto da Jon Favreau. Tony Stark (Rob
 $plot_3 = "La lotta tra il bene (gli Autobots) e il male (i Decepticons), dal pianeta Cybertron si è spostata sulla Terra dove milioni di anni fa è caduto il Cubo di Energon, il potere supremo capace di infondere la vita ai Transformers. Sam Witwicky - nipote dell'esploratore che per primo, durante una missione nel Circolo Polare Artico sul finire del 1800, ebbe a che fare con Megatron, il capo dei Decepticons - è l'unico che può aiutare Optimus Prime e i suoi Autobots a ritrovare il cubo e distruggerlo prima che finisca nelle mani dei nemici.";
 
 // Creo un movie1
-$movie_1 = new Movie('Spider-Man', $plot_1, 'Action/Fantasy', 2003, $director_1);
+$movie_1 = new Movie('Spider-Man', $plot_1, 'Action/Fantasy', 2002, $director_1, $main_actor_1);
 // var_dump($movie_1);
 
 // Creo un movie2
-$movie_2 = new Movie('Iron Man', $plot_2, 'Action/Fantasy', 2008, $director_2);
+$movie_2 = new Movie('Iron Man', $plot_2, 'Action/Fantasy', 2008, $director_2, $main_actor_2);
 // var_dump($movie_2);
 
 // Creo un movie3
-$movie_3 = new Movie('Transformers', $plot_3, 'Action/Fantasy', 2007, $director_3);
+$movie_3 = new Movie('Transformers', $plot_3, 'Action/Fantasy', 2007, $director_3, $main_actor_3);
 // var_dump($movie_3);
